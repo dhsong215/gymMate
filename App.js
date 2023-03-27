8cd7151342397eaedf35a03d3cf62d5f4e5175b1
dhsong215 <77286910+dhsongdev@users.noreply.github.com>
2023-03-28 03:34:30 +0900

    add google signin

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-import React, {useContext, useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {useColorScheme} from 'react-native';
+import {auth} from './src/firebase';
 
 import {themeColorsContext, userContext} from './src/contexts';
 import {darkModeColors, lightModeColors} from './src/styles/themeColors';
@@ -9,8 +10,33 @@ import InStack from './src/navigation/InStack';
 import OutStack from './src/navigation/OutStack';
 
 export default function App() {
+  const [appLoading, setAppLoading] = useState(true);
   const [user, setUser] = useState();
   const colorScheme = useColorScheme();
+
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (appLoading) setAppLoading(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged); //onAuthStateChanged에 유저 정보 인자를 전달합니다.
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (appLoading) return null;
+
+  if (!user) {
+    return (
+      <themeColorsContext.Provider
+        value={colorScheme === 'dark' ? darkModeColors : lightModeColors}>
+        <NavigationContainer>
+          <OutStack />
+        </NavigationContainer>
+      </themeColorsContext.Provider>
+    );
+  }
+
   return (
     <userContext.Provider value={user}>
       <themeColorsContext.Provider
